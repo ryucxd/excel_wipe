634ef19e859cb6f2e4fe6cb7a2bca62581e0d23f
ryucxd <ryucxd@gmail.com>
2022-08-30 10:34:04 +0100

    Add project files.

---
+++ excel_wipe/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excel_wipe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Process[] process = System.Diagnostics.Process.GetProcessesByName("MSACCESS.EXE");
+            foreach (System.Diagnostics.Process p in process)
+            {
+                if (!string.IsNullOrEmpty(p.ProcessName))
+                {
+                    try
+                    {
+                        p.Kill(); //kills each process :}
+                    }
+                    catch { }
+                }
+            }
+        }
+    }
+}
